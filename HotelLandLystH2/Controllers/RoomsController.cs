505b0fb635959cb91713be27b52d0ad288e21e5a
Kenneth Folke Larsen <KennethLarsenBusiness@hotmail.com>
2020-11-23 12:26:43 +0100

    Add project files.

---
+++ HotelLandLystH2/Controllers/RoomsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelLandLystH2.Controllers
+{
+    public class RoomsController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            //Uses HtmlEncoder.Default.Encode to protect the app from malicious input (namely JavaScript).
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+    }
+}
